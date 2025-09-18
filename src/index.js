9f582a59f351a21d0ae550d77fd79c057fd41939
Jean Kuyk <jeankuyk7@gmail.com>
2025-09-18 15:19:18 -0500

    added gradient low highlight text format

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Experimental Gradient Highlight format for RichText in Gutenberg
 import {
     registerFormatType,
     applyFormat,
@@ -5,12 +6,73 @@ import {
 } from "@wordpress/rich-text";
 import { __ } from "@wordpress/i18n";
 import { RichTextToolbarButton, ColorPalette } from "@wordpress/block-editor";
-import { useState } from "@wordpress/element";
-import { Popover, PanelBody } from "@wordpress/components";
+import { PanelBody, Popover, GradientPicker } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import lowHighlightIcon from "./assets/low-highlight.svg";
 import lowHighlightActive from "./assets/low-highlight-active.svg";
-import "./style.scss";
+import './style.scss';
 
+registerFormatType(
+    'blockylicious/low-highlight-gradient',
+    {
+        title: __("Gradient highlight", "blockylicious"),
+        tagName: 'span',
+        className: 'blockylicious-low-highlight-gradient', // class to apply the gradient and lower position. defined in style.scss
+        edit: ({ onChange, value, contentRef, isActive }) => {
+            const [showGradients, setShowGradients] = useState(false);
+            const lowHighlightGrad = value.activeFormats?.find(
+                (format) => format.type === "blockylicious/low-highlight-gradient"
+            );
+            const attributes = {
+                ...(lowHighlightGrad?.attributes || {}),
+                ...(lowHighlightGrad?.unregisteredAttributes || {}),
+            };
+            console.log({ lowHighlightGrad });
+
+            return (
+                <>
+                    <RichTextToolbarButton
+                        icon="art"
+                        title={__("Low Highlight Gradient", "blockylicious")}
+                        onClick={() => setShowGradients(!showGradients)}
+                        isActive={isActive}
+                    />
+                    {showGradients && (
+                        <Popover
+                            anchor={contentRef?.current}
+                            onClose={() => setShowGradients(false)}
+                        >
+                            <PanelBody>
+                                <GradientPicker
+                                    label={__("Highlight Gradient", "blockylicious")}
+                                    value={attributes?.["data-gradient"]}
+                                    onChange={(newGradient) => {
+                                        if (newGradient) {
+                                            onChange(
+                                                applyFormat(value, {
+                                                    type: "blockylicious/low-highlight-gradient",
+                                                    attributes: {
+                                                        "data-gradient": newGradient,
+                                                        style: `background-image: ${newGradient}`,
+                                                    },
+                                                })
+                                            );
+                                        } else {
+                                            onChange(
+                                                removeFormat(value, "blockylicious/low-highlight-gradient")
+                                            );
+                                        }
+                                    }}
+                                />
+                            </PanelBody>
+                        </Popover>
+                    )}
+                </>
+            );
+        },
+    }
+);
+// Solid low highlight format (original)
 registerFormatType("blockylicious/low-highlight", {
     title: __("Low highlight", "blockylicious"),
     tagName: "span",
@@ -25,6 +87,7 @@ registerFormatType("blockylicious/low-highlight", {
             ...(lowHighlight?.unregisteredAttributes || {}),
         };
         console.log({ lowHighlight });
+
         return (
             <>
                 <RichTextToolbarButton
@@ -79,5 +142,7 @@ registerFormatType("blockylicious/low-highlight", {
                 )}
             </>
         );
-    },
-});
+    }
+}
+);
+
